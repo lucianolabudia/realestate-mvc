fdcb00d8d57a2239da6e7c379b6679da46304b4f
Luciano Labudia <labudia.luciano@gmail.com>
2023-02-18 12:33:01 -0300

    Creation of registration, authentication and forgotten password pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,24 @@
 
 const formLogin = (req, res) => {
     res.render('auth/login', {
-        
+        page: 'Iniciar Sesion'
     })
 }
 
 const formRegister = (req, res) => {
     res.render('auth/register', {
-        
+        page: 'Crear Cuenta'
+    })
+}
+
+const recoveryForm = (req, res) => {
+    res.render('auth/recovery', {
+        page: 'Recupera tu acceso a Real Estate'
     })
 }
 
 export {
     formLogin,
-    formRegister
+    formRegister,
+    recoveryForm
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { formLogin, formRegister } from "../controllers/userController.js";
+import { formLogin, formRegister, recoveryForm } from "../controllers/userController.js";
 
 const router = express.Router();
 
 
 router.get('/login', formLogin);
 router.get('/registro', formRegister);
+router.get('/recuperar-cuenta', recoveryForm);
 
-
+       
 export default router;
